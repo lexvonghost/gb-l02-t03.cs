35720290bf23253e67460d99de904eb6fd600159
lexvonghost <lex_von_tost@mail.ru>
2022-07-24 21:16:35 +0300

    feat: решение задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+string str = n.ToString();
+if (str.Length > 2)
+{
+    Console.WriteLine(str[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+
