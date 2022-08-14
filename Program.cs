0171c24bdd17b26249d1e962445de02250b19557
lexvonghost <lex_von_tost@mail.ru>
2022-08-14 14:11:57 +0300

    build: добавил условия

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите число");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
 int n = Convert.ToInt32(Console.ReadLine());
 string str = n.ToString();
 if (str.Length > 2)
